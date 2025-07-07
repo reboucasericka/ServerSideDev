02f24786d35817a4786b4dc2068836ca01138886
reboucasericka <reboucasericka@gmail.com>
2025-07-07 20:32:31 +0100

    Create database and countries table

---
+++ ServerSideDev.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServerSideDev.Web.Data.Entities;
+
+namespace ServerSideDev.Web.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) 
+		{ 
+		}
+
+		public DbSet<Country> countries { get; set; }
+	}
+}
